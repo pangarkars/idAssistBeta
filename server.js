3559507c8406dc49d062e21da1c187b4bb64520f
Meenal Pangarkar <Meenal.pangarkar@vida.id>
2022-02-25 15:31:31 +0530

    added headers

--- server.js
+++ server.js
@@ -1,30 +1,13 @@
 const express = require("express");
 const path = require("path");
-//const Heroku = require("heroku-client");
-//const heroku = new Heroku({ token: process.env.API_TOKEN });
 const app = express();
 
 let SECRET_KEY = "";
 
 app.use(express.static(__dirname + "/dist/id-assist"));
 
-/* heroku
-  .request({
-    method: "GET",
-    path: "https://api.heroku.com/apps/name-of-app/config-vars",
-    headers: {
-      Accept: "application/vnd.heroku+json; version=3",
-      Authorization: "Bearer " + process.env.API_TOKEN,
-    },
-    parseJSON: true,
-  })
-  .then((response) => {
-    console.log(response, ">>>>>>heroku api...");
-    SECRET_KEY = response.OPENAI_SECRET_KEY;
-  }); */
-
-app.get("", function (req, res) {
-  res.json(TOKEN);
+app.get("/backend", (req, res) => {
+  res.json({ url: process.env.BACKEND_URL });
 });
 
 app.get("/*", function (req, res) {
